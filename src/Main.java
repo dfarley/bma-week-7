9409eead67ecea49b3158907934d215fe8b71656
dfarley <david@crossfireconsulting.com>
2012-10-21 15:06:40 -0400

    bad catch exception

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 
 public class Main {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		try {
 			int i = 1/0;
 			System.out.println(i);
 		}
-		catch(Exception e) {}
+		catch(Exception e) {
+			log(e);
+		}
+		catch(ArithmeticException e) {
+			log(e);
+		}
 	}
 
+	public static void log(Exception e) {
+		System.out.println("Logged: "+e);
+	}
 }
